a1b769e7aecfbe77adadaa020d69d28123f7b8dc
Robert Cimrman <cimrman3@ntc.zcu.cz>
2025-10-05 19:19:06 +0200

    new type_as()

--- elfpy/base.py
+++ elfpy/base.py
@@ -7,6 +7,16 @@ from soops import Output
 
 _dashes = '-'*50
 
+def type_as(arg, type_arg):
+    try:
+        out = type(type_arg)(arg)
+
+    except ValueError:
+        msg = 'argument "%s" cannot be converted to type %s!'
+        raise ValueError(msg % (arg, type(type_arg)))
+
+    return out
+
 class Object(object):
     traits = {}
 
