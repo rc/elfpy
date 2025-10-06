2b6e6f9e031f25328d443e5faecaad2bf122b785
Robert Cimrman <cimrman3@ntc.zcu.cz>
2025-10-06 21:40:17 +0200

    new elfpy/tm_mtl32_2020.py - new MTL32_2020
    - new .read_data(), convert(), .get_init_length()

---
+++ elfpy/tm_mtl32_2020.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+from .devices import TestingMachine
+
+class MTL32_2020(TestingMachine):
+    """
+    TestResources Inc. MTL32_2020 controller.
+    """
+    name = 'mtl32_2020'
+    converted_columns = dict(time=1, displ=7, force=4, cycle=2)
+
+    def read_data(self, filename):
+        mdf = pd.read_csv(filename, skiprows=5)
+        mdf = mdf.rename(columns=lambda x: x.strip())
+        return mdf
+
+    def convert(self, mdf):
+        """
+        Convert to the format suitable for`elfpy-process`.
+        """
+        df = pd.DataFrame()
+        df['Time [s]'] = mdf['Time sec']
+        df['Cycle'] = mdf['CY-X1']
+        df['Force1 [N]'] = mdf['X1L N']
+        df['Force2 [N]'] = mdf['X2L N']
+        df['Force [N]'] = np.minimum(df['Force1 [N]'], df['Force2 [N]'])
+        df['Displacement [mm]'] = mdf['X1Disp mm'] + mdf['X2Disp mm']
+        df['Elongation [mm]'] = (df['Displacement [mm]']
+                                 - df['Displacement [mm]'][0])
+        return df
+
+    def get_init_length(self, mdf):
+        length = mdf.loc[0, 'X1Disp mm'] + mdf.loc[0, 'X2Disp mm']
+        return length
